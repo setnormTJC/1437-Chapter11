2a027233b6d467e9eec61bbda2c1baf9c4ec7a44
Work <Work@MSI>
2024-03-20 15:09:23 -0500

    Where we ended on Mar. 20

--- AnAnalogOfLab11c/House.h
+++ AnAnalogOfLab11c/House.h
@@ -2,18 +2,70 @@
 
 #include <string> 
 
-class House
-{
-	Window frontWindow; 
-	vector<Door> doors; 
+#include<vector> 
 
-	Door doors2[2];
+#include<iostream>
+
+using namespace std; 
+
+struct Window
+{
+	int windowLength = 42; //inches
+
+	double price; 
+
+	void applyTint()
+	{
+		cout << "Tint has been applied - now you don't have to squint so much." << endl; 
+	}
+
+	void openWindow()
+	{
+		cout << "Opening window ... it feels TERRIFFIC outside!\n"; 
+	}
+
 };
 
-
-class Window
+class Door
 {
 public: 
-	int windowLength = 42; 
-	std::string windowOpacity = "1 opaqueness units";
+	string doorColor = "brown"; 
+
+	void paintDoor(string doorColor) //setter function 
+	{
+		Door::doorColor = doorColor; 
+	}
 };
+
+struct Person
+{
+	string name; 
+
+	int height; 
+
+	void increaseHeight(int heightIncreaseAmount)
+	{
+		cout << "You put on high heels that are " << heightIncreaseAmount
+			<< "inches tall - now your height is: " << endl; 
+		
+		height = height + heightIncreaseAmount; 
+
+		cout << height << endl; 
+	}
+};
+
+class House
+{
+public: 
+	Window frontWindow; 
+	//vector<Door> doors; 
+
+	Door doors[2];
+
+	Person you; 
+
+
+};
+
+
+
--- AnAnalogOfLab11c/Lab11cAnalog.cpp
+++ AnAnalogOfLab11c/Lab11cAnalog.cpp
@@ -9,10 +9,15 @@ int main()
 
     House yourHouse; 
     //house consists of 1 window, 2 doors, and a SINGLE person 
+    
+    //yourHouse.doStuff1();
+    yourHouse.doors[0].paintDoor("pink");
 
-    yourHouse.doStuff1();
+    cout << "You have now painted your door this color: "
+        << yourHouse.doors[0].doorColor << endl; 
 
-    yourHouse.doStuff2();
+    //yourHouse.doStuff2();
+ 
 
 
     return 0;
